1ff632c4585a67daf58594bcf71a11c8268a0695
AitorMelero <meleropiconaitor@gmail.com>
2024-01-31 19:16:16 +0100

    Feat: Incluir propiedad y color para casilla seleccionada
    He incluido la propiedad isSelected y el color para las casillas seleccionadas.

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,13 +1,19 @@
+import { useState } from 'react'
+
 interface Props {
   isWhite: boolean
 }
 
 export const Square: React.FC<Props> = ({ isWhite }) => {
+  const [isSelected, setIsSelected] = useState(false)
   const bgColor = `${isWhite ? 'bg-light-square' : 'bg-dark-square'}`
+  const bgSelectedColor = `bg-selected-square ${isSelected ? 'bg-opacity-60' : 'bg-opacity-0'}`
 
   return (
     <div className={bgColor}>
-      Square
+      <div className={bgSelectedColor}>
+        Square
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ export default {
     extend: {
       colors: {
         'dark-square': '#779952',
-        'light-square': '#edeed1'
+        'light-square': '#edeed1',
+        'selected-square': '#ffff33'
       }
     },
   },
