ad3871db12ca618e19d85fe760428fd0ac5a2ec7
AitorMelero <meleropiconaitor@gmail.com>
2024-02-01 13:06:57 +0100

    Test: Probar componente Square
    Los tests para el componente Square prueban si se pintan las casillas como corresponde y si se muestran o no las coordenadas como corresponde.

--- tests/components/Square.spec.tsx
+++ tests/components/Square.spec.tsx
@@ -19,4 +19,40 @@ test.describe('Square test', () => {
 
     await expect(whiteSquare).toHaveClass(bgLightClass)
   })
+
+  test('comprobar si se muestra la coordenada númerica de la casilla', async ({ mount }) => {
+    const numberCoordinate = 2
+    const square = await mount(
+      <Square xPosition={1} yPosition={numberCoordinate} />
+    )
+
+    await expect(square).toContainText(numberCoordinate.toString())
+  })
+
+  test('comprobar si no se muestra la coordenada númerica de la casilla', async ({ mount }) => {
+    const numberCoordinate = 2
+    const square = await mount(
+      <Square xPosition={2} yPosition={numberCoordinate} />
+    )
+
+    await expect(square).not.toContainText(numberCoordinate.toString())
+  })
+
+  test('comprobar si se muestra la coordenada letra de la casilla', async ({ mount }) => {
+    const letterCoordinate = 2
+    const square = await mount(
+      <Square xPosition={letterCoordinate} yPosition={1} />
+    )
+
+    await expect(square).toContainText(String.fromCharCode(letterCoordinate + 96))
+  })
+
+  test('comprobar si no se muestra la coordenada letra de la casilla', async ({ mount }) => {
+    const letterCoordinate = 2
+    const square = await mount(
+      <Square xPosition={letterCoordinate} yPosition={2} />
+    )
+
+    await expect(square).not.toContainText(String.fromCharCode(letterCoordinate + 96))
+  })
 })
