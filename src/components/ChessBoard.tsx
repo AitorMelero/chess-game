8eb627e471a7ec83811de74fb77ec9d01a84a8c2
AitorMelero <meleropiconaitor@gmail.com>
2024-02-01 15:52:27 +0100

    Feat: Incluir hook useSquarePositions

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -1,24 +1,14 @@
+import { useSquarePositions } from '../hooks/useSquarePositions'
 import { Square } from './Square'
 
-interface squarePositions {
-  xPosition: number
-  yPosition: number
-}
-
 export const ChessBoard: React.FC = () => {
-  const squarePositions: squarePositions[] = []
-
-  for (let y = 8; y > 0; y--) {
-    for (let x = 1; x <= 8; x++) {
-      squarePositions.push({ xPosition: x, yPosition: y })
-    }
-  }
+  const { squarePositions } = useSquarePositions()
 
   return (
     <div className='grid grid-cols-8 w-1/2 h-[66vh]'>
       {
         squarePositions.map(({ xPosition, yPosition }) => (
-          <Square key={xPosition + yPosition} xPosition={xPosition} yPosition={yPosition} />
+          <Square key={xPosition.toString() + yPosition.toString()} xPosition={xPosition} yPosition={yPosition} />
         ))
       }
     </div>
