38d8ef7431eb1255b1fffcb71a2c595982c7f19b
AitorMelero <meleropiconaitor@gmail.com>
2024-02-06 20:00:30 +0100

    Chore: Configurar entorno de testing con Playwright
    Nota: Con la versión menor de 20.11 de Node hay un bug.

---
+++ tests/model/PieceModel.spec.ts
@@ -0,0 +1,7 @@
+import { expect, test } from '@playwright/test'
+
+test.describe('PieceModel Testing', () => {
+  test('deberían asignarse correctamente los atributos durante la creación', () => {
+    expect(true).toBeTruthy()
+  })
+})
