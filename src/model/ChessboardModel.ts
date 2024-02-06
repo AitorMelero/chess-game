4d217facac0bbdff12595b61cdcb3ba965739e63
AitorMelero <meleropiconaitor@gmail.com>
2024-02-06 12:37:03 +0100

    Chore: Limpiar antiguo ChessBoard

--- src/model/ChessboardModel.ts
+++ src/model/ChessboardModel.ts
@@ -1,6 +1,7 @@
 import { type ChessboardModelType } from '../types/Chessboard'
 import { type PieceModelType } from '../types/Piece'
 import { type SquareModelType } from '../types/Square'
+import { SquareModel } from './SquareModel'
 
 export class ChessboardModel implements ChessboardModelType {
   readonly #squares: SquareModelType[]
@@ -9,10 +10,24 @@ export class ChessboardModel implements ChessboardModelType {
   readonly #pieces: PieceModelType[]
 
   constructor () {
-    this.#squares = []
+    this.#squares = this.getInitSquares()
     this.#possibleNextSquares = []
     this.#currentPiece = null
     this.#pieces = []
+  }
+
+  private getInitSquares (): SquareModelType[] {
+    const CHESSBOARD_LEN = 8
+    const squares: SquareModelType[] = []
+
+    // Create squares from left top to right bottom
+    for (let y = CHESSBOARD_LEN; y > 0; y--) {
+      for (let x = 1; x <= CHESSBOARD_LEN; x++) {
+        squares.push(new SquareModel(x, y))
+      }
+    }
+
+    return squares
   }
 
   get squares (): SquareModelType[] {
