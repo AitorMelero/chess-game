4db583c36480b985a9157073e7b8046d16041cb6
AitorMelero <meleropiconaitor@gmail.com>
2024-02-07 09:52:34 +0100

    Test: Testar getters y setters basicos de los objetos Piece

--- tests/model/PieceModel.spec.ts
+++ tests/model/PieceModel.spec.ts
@@ -1,7 +1,41 @@
 import { expect, test } from '@playwright/test'
+import { PawnModel } from '../../src/model/PawnModel'
+import { SquareModel } from '../../src/model/SquareModel'
 
 test.describe('PieceModel Testing', () => {
-  test('deberían asignarse correctamente los atributos durante la creación', () => {
-    expect(true).toBeTruthy()
+  // Pawn piece as example
+  const isWhite = true
+  const whiteImage = './src/assets/Pieces/wp.png'
+  const blackImage = './src/assets/Pieces/bp.png'
+  const whitePiece = new PawnModel(isWhite)
+  const blackPiece = new PawnModel(!isWhite)
+  const xPosition = 1
+  const yPosition = 1
+  const squareModel = new SquareModel(xPosition, yPosition)
+
+  test('debería devolver el valor isWhite correcto', () => {
+    expect(whitePiece.isWhite).toBeTruthy()
+    expect(blackPiece.isWhite).toBeFalsy()
+  })
+
+  test('debería devolver el path de la imagen correcta', () => {
+    expect(whitePiece.image).toStrictEqual(whiteImage)
+    expect(blackPiece.image).toStrictEqual(blackImage)
+  })
+
+  test('debería devolver el square correcto', () => {
+    expect(whitePiece.square).toBeUndefined()
+    expect(blackPiece.square).toBeUndefined()
+
+    whitePiece.square = squareModel
+    blackPiece.square = squareModel
+  })
+
+  test('debería modificar el square de manera correcta', () => {
+    whitePiece.square = squareModel
+    blackPiece.square = squareModel
+
+    expect(whitePiece.square).toStrictEqual(squareModel)
+    expect(blackPiece.square).toStrictEqual(squareModel)
   })
 })
