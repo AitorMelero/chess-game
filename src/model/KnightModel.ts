cd262dc84c5ee17cc6ba4ae5a7b7e6dd6c5860f6
AitorMelero <meleropiconaitor@gmail.com>
2024-02-08 16:23:52 +0100

    Feat: Implementar movimiento del caballo

--- src/model/KnightModel.ts
+++ src/model/KnightModel.ts
@@ -1,3 +1,4 @@
+import { type SquarePosition, type SquareModelType } from '../types/Square'
 import { PieceModel } from './PieceModel'
 
 export class KnightModel extends PieceModel {
@@ -6,5 +7,59 @@ export class KnightModel extends PieceModel {
     const blackKnightImage = './src/assets/Pieces/bn.png'
     const image = isWhite ? whiteKnightImage : blackKnightImage
     super(image, isWhite)
+  }
+
+  calculatePossibleNextSquares (): SquareModelType[] {
+    const possibleNextSquares: SquareModelType[] = []
+
+    if (this.square !== undefined) {
+      const possiblePosition: SquarePosition[] = [
+        {
+          xPosition: this.square.xPosition - 2,
+          yPosition: this.square.yPosition + 1
+        },
+        {
+          xPosition: this.square.xPosition - 1,
+          yPosition: this.square.yPosition + 2
+        },
+        {
+          xPosition: this.square.xPosition + 1,
+          yPosition: this.square.yPosition + 2
+        },
+        {
+          xPosition: this.square.xPosition + 2,
+          yPosition: this.square.yPosition + 1
+        },
+        {
+          xPosition: this.square.xPosition + 2,
+          yPosition: this.square.yPosition - 1
+        },
+        {
+          xPosition: this.square.xPosition + 1,
+          yPosition: this.square.yPosition - 2
+        },
+        {
+          xPosition: this.square.xPosition - 1,
+          yPosition: this.square.yPosition - 2
+        },
+        {
+          xPosition: this.square.xPosition - 2,
+          yPosition: this.square.yPosition - 1
+        }
+      ]
+
+      const chessboard = this.square.chessboard
+      possiblePosition.forEach(position => {
+        const square = chessboard.getSquareFromPosition(position)
+        if (square !== undefined) {
+          const pieceInSquare = square.piece
+          if (pieceInSquare === undefined || pieceInSquare.isWhite !== this.isWhite) {
+            possibleNextSquares.push(square)
+          }
+        }
+      })
+    }
+
+    return possibleNextSquares
   }
 }
