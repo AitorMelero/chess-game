f61e7726e19ec114117984060e522f90bf95c679
AitorMelero <meleropiconaitor@gmail.com>
2024-02-08 11:47:15 +0100

    Feat: Implementar movimiento del peón a falta de comer piezas

--- src/model/PawnModel.ts
+++ src/model/PawnModel.ts
@@ -9,13 +9,67 @@ export class PawnModel extends PieceModel {
     super(image, isWhite)
   }
 
+  private isSquareInPossibleMove (square: SquareModelType): boolean {
+    const isFirstMove = this.isWhite ? this.square?.yPosition === 2 : this.square?.yPosition === 7
+    const nextYPossibleMove = this.calculateNextYPossibleMoves(isFirstMove)
+
+    return nextYPossibleMove.find(
+      yPosition => yPosition === square.yPosition &&
+      this.square?.xPosition === square.xPosition
+    ) !== undefined
+  }
+
+  private calculateNextYPossibleMoves (isFirstMove: boolean): number[] {
+    const yPositions: number[] = []
+
+    if (this.square !== undefined) {
+      if (this.isWhite) {
+        yPositions.push(this.square.yPosition + 1)
+        if (isFirstMove) {
+          yPositions.push(this.square.yPosition + 2)
+        }
+      } else {
+        yPositions.push(this.square.yPosition - 1)
+        if (isFirstMove) {
+          yPositions.push(this.square.yPosition - 2)
+        }
+      }
+    }
+
+    return yPositions
+  }
+
+  private pawnCanEatPiece (squareTarget: SquareModelType): boolean {
+    let pawnCanEat = false
+
+    if (squareTarget.piece?.isWhite !== this.isWhite && this.square !== undefined) {
+      if (this.isWhite) {
+        if (squareTarget.yPosition === this.square.yPosition + 1) {
+          if (squareTarget.xPosition === this.square.xPosition - 1 || squareTarget.xPosition === this.square.xPosition + 1) {
+            pawnCanEat = true
+          }
+        }
+      } else {
+        if (squareTarget.yPosition === this.square.yPosition - 1) {
+          if (squareTarget.xPosition === this.square.xPosition - 1 || squareTarget.xPosition === this.square.xPosition + 1) {
+            pawnCanEat = true
+          }
+        }
+      }
+    }
+
+    return pawnCanEat
+  }
+
   calculatePossibleNextSquares (): SquareModelType[] {
     const currentSquare = this.square
     let nextPossibleSquares: SquareModelType[] = []
 
     if (currentSquare !== undefined) {
       nextPossibleSquares = currentSquare.chessboard.squares.filter(
-        square => square.yPosition === currentSquare.yPosition + 1 && square.xPosition === currentSquare.xPosition
+        square =>
+          (square.piece !== undefined && square.piece.isWhite !== this.isWhite && this.pawnCanEatPiece(square)) ||
+          (this.isSquareInPossibleMove(square) && square.piece === undefined)
       )
     }
 
