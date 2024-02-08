33d7cbc31a680365d53adf2f1463cdf581c5313c
AitorMelero <meleropiconaitor@gmail.com>
2024-02-08 19:47:48 +0100

    Feat: Implementar movimiento del rey sin jugadas especiales

--- src/model/KingModel.ts
+++ src/model/KingModel.ts
@@ -1,3 +1,5 @@
+import { getBishopNextPossibleMoves, getRookNextPossibleMoves } from '../helpers'
+import { type SquareModelType } from '../types/Square'
 import { PieceModel } from './PieceModel'
 
 export class KingModel extends PieceModel {
@@ -6,5 +8,31 @@ export class KingModel extends PieceModel {
     const blackKingImage = './src/assets/Pieces/bk.png'
     const image = isWhite ? whiteKingImage : blackKingImage
     super(image, isWhite)
+  }
+
+  calculatePossibleNextSquares (): SquareModelType[] {
+    let nextPossibleSquares: SquareModelType[] = []
+
+    if (this.square !== undefined) {
+      const pieceSquare = this.square
+
+      nextPossibleSquares = [
+        ...getRookNextPossibleMoves(this.square),
+        ...getBishopNextPossibleMoves(this.square)
+      ].filter(square => {
+        return (
+          (square.xPosition === pieceSquare.xPosition && square.yPosition === pieceSquare.yPosition + 1) ||
+          (square.xPosition === pieceSquare.xPosition + 1 && square.yPosition === pieceSquare.yPosition + 1) ||
+          (square.xPosition === pieceSquare.xPosition + 1 && square.yPosition === pieceSquare.yPosition) ||
+          (square.xPosition === pieceSquare.xPosition + 1 && square.yPosition === pieceSquare.yPosition - 1) ||
+          (square.xPosition === pieceSquare.xPosition && square.yPosition === pieceSquare.yPosition - 1) ||
+          (square.xPosition === pieceSquare.xPosition - 1 && square.yPosition === pieceSquare.yPosition - 1) ||
+          (square.xPosition === pieceSquare.xPosition - 1 && square.yPosition === pieceSquare.yPosition) ||
+          (square.xPosition === pieceSquare.xPosition - 1 && square.yPosition === pieceSquare.yPosition + 1)
+        )
+      })
+    }
+
+    return nextPossibleSquares
   }
 }
