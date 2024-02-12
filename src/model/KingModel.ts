cd6b4b0da8b7548609b033b2169adce4065fa1bf
AitorMelero <meleropiconaitor@gmail.com>
2024-02-12 14:10:15 +0100

    Feat: Empezar a implementar el enroque

--- src/model/KingModel.ts
+++ src/model/KingModel.ts
@@ -3,11 +3,56 @@ import { type SquareModelType } from '../types/Square'
 import { PieceModel } from './PieceModel'
 
 export class KingModel extends PieceModel {
+  #isFirstMove: boolean
+
   constructor (isWhite: boolean) {
     const whiteKingImage = './src/assets/Pieces/wk.png'
     const blackKingImage = './src/assets/Pieces/bk.png'
     const image = isWhite ? whiteKingImage : blackKingImage
     super(image, isWhite)
+    this.#isFirstMove = true
+  }
+
+  get isFirstMove (): boolean {
+    return this.#isFirstMove
+  }
+
+  set isFirstMove (isFirstMove: boolean) {
+    this.#isFirstMove = isFirstMove
+  }
+
+  private isShortCastling (): boolean {
+    return false
+  }
+
+  private getShortCastlingSquares (): SquareModelType[] {
+    const shortCastlingSquares: SquareModelType[] = []
+    let castilingSquare1: SquareModelType | undefined
+    let castilingSquare2: SquareModelType | undefined
+
+    if (this.square !== undefined) {
+      if (this.isWhite) {
+        castilingSquare1 = this.square.chessboard.getSquareFromPosition({ xPosition: 6, yPosition: 1 })
+        castilingSquare2 = this.square.chessboard.getSquareFromPosition({ xPosition: 7, yPosition: 1 })
+        if (castilingSquare1 !== undefined && castilingSquare2 !== undefined) {
+          shortCastlingSquares.push(castilingSquare1)
+          shortCastlingSquares.push(castilingSquare2)
+        }
+      } else {
+        castilingSquare1 = this.square.chessboard.getSquareFromPosition({ xPosition: 6, yPosition: 8 })
+        castilingSquare2 = this.square.chessboard.getSquareFromPosition({ xPosition: 7, yPosition: 8 })
+        if (castilingSquare1 !== undefined && castilingSquare2 !== undefined) {
+          shortCastlingSquares.push(castilingSquare1)
+          shortCastlingSquares.push(castilingSquare2)
+        }
+      }
+    }
+
+    return shortCastlingSquares
+  }
+
+  private isLongCastling (): boolean {
+    return false
   }
 
   calculatePossibleNextSquares (): SquareModelType[] {
@@ -22,5 +67,21 @@ export class KingModel extends PieceModel {
     }
 
     return nextPossibleSquares
+  }
+
+  isInCheck (): boolean {
+    let isCheck = false
+
+    if (this.square !== undefined) {
+      isCheck = this.square.chessboard.pieces.filter(
+        piece => !(piece instanceof KingModel) && piece.calculatePossibleNextSquares().find(
+          square => square === this.square
+        )
+      ).length > 0
+    }
+
+    console.log(isCheck)
+
+    return isCheck
   }
 }
--- src/model/RookModel.ts
+++ src/model/RookModel.ts
@@ -3,11 +3,22 @@ import { type SquareModelType } from '../types/Square'
 import { PieceModel } from './PieceModel'
 
 export class RookModel extends PieceModel {
+  #isFirstMove: boolean
+
   constructor (isWhite: boolean) {
     const whiteRookImage = './src/assets/Pieces/wr.png'
     const blackRookImage = './src/assets/Pieces/br.png'
     const image = isWhite ? whiteRookImage : blackRookImage
     super(image, isWhite)
+    this.#isFirstMove = true
+  }
+
+  get isFirstMove (): boolean {
+    return this.#isFirstMove
+  }
+
+  set isFirstMove (isFirstMove: boolean) {
+    this.#isFirstMove = isFirstMove
   }
 
   calculatePossibleNextSquares (): SquareModelType[] {
